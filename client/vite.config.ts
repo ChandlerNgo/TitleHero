03d3cb58a6fe2ebedb898cf4229af3a7a910a0d0
Chandler Ngo <chandlerngo00@gmail.com>
2025-11-04 12:18:51 -0600

    changed the env upload system

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
-// Load environment variables from .env if it exists
-dotenv.config()
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-// Set default target if environment variable is not defined
-const target = process.env.VITE_API_TARGET || 'https://5mj0m92f17.execute-api.us-east-2.amazonaws.com/'
+  const target = env.VITE_API_TARGET || 'https://5mj0m92f17.execute-api.us-east-2.amazonaws.com/'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target,
-        changeOrigin: true,
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target,
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  })
+}
