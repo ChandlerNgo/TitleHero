1160e37e170b35d578c7a907521ab1d42a063a6a
thequ3so <caseys@starfish-studios.com>
2025-11-04 11:28:07 -0600

    Fixed Search on deployed environment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
-// https://vite.dev/config/
+// Load environment variables from .env if it exists
+dotenv.config()
+
+// Set default target if environment variable is not defined
+const target = process.env.VITE_API_TARGET || 'https://5mj0m92f17.execute-api.us-east-2.amazonaws.com/'
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target,
         changeOrigin: true,
       },
     },
   },
-
 })
